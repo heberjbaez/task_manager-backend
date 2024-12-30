0cc4429918f8efca2a52f582b8c166364dfc637a
Heber <heberjbaez@gmail.com>
2024-12-30 18:22:15 -0300

    Add cors-dep

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ const express = require("express")
 const mongoose = require("mongoose")
 const taskRoutes = require("./routes/task.routes")
 const bodyParser = require("body-parser")
+const cors = require("cors")
 
 process.loadEnvFile()
 const port = process.env.PORT || 3000
 
 const app = express()
+
+app.use(cors({ origin: "http://localhost:5173" }))
 app.use(bodyParser.json())
 
 mongoose.connect(process.env.MONGO_URL, { dbName: process.env.MONGO_DB_NAME })
